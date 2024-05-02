4732897f6f2266b23fd67a01683e41d1a159fb17
Mehmet <mathdenizi@gmail.com>
2024-05-02 03:48:55 +0200

    using tokens update delete and create place

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -146,6 +146,7 @@ const login = async (req, res, next) => {
     );
     return next(error);
   }
+  console.log(token);
   res.json({
     userId: existingUser.id,
     email: existingUser.email,
--- middleware/chack-auth.js
+++ middleware/chack-auth.js
@@ -1,5 +1,7 @@
 const jwt = require("jsonwebtoken");
+
 const HttpError = require("../models/http-error");
+
 module.exports = (req, res, next) => {
   if (req.method === "OPTIONS") {
     return next();
@@ -7,13 +9,13 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1]; // Authorization: 'Bearer TOKEN'
     if (!token) {
-      throw new Error("Authentication failed");
+      throw new Error("Authentication failed!");
     }
     const decodedToken = jwt.verify(token, "supersecret_dont_share");
     req.userData = { userId: decodedToken.userId };
     next();
   } catch (err) {
-    const error = new HttpError("Authentication failed", 401);
+    const error = new HttpError("Authentication failed!", 403);
     return next(error);
   }
 };
